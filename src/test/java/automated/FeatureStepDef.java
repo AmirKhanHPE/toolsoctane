bf543dcb95eafb419ee0b0914a09ac3cb3534d8f
khanami <amir.khan@hpe.com>
2017-05-06 14:18:49 +0200

    user story #110005: As an admin i want to create my own query scripts for data collection task #58007: Use VBS for data collection

--- src/test/java/automated/FeatureStepDef.java
+++ src/test/java/automated/FeatureStepDef.java
@@ -3,6 +3,8 @@ package automated;
 import com.hp.lft.report.Reporter;
 import com.hp.lft.sdk.ModifiableSDKConfiguration;
 import com.hp.lft.sdk.SDK;
+import com.hp.lft.sdk.web.*;
+import com.hp.lft.verifications.Verify;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -14,6 +16,7 @@ import java.net.URI;
  * Created by khanami on 04.05.2017.
  */
 public class FeatureStepDef extends UnitTestClassBase{
+    Browser browser;
 
     @Given("^user is logged in$")
     public void user_is_logged_in() throws Throwable {
@@ -23,6 +26,12 @@ public class FeatureStepDef extends UnitTestClassBase{
         SDK.init(config);
         Reporter.init();
         Reporter.reportEvent("Step executed", "step executed");
+        browser = BrowserFactory.launch(BrowserType.CHROME);
+        browser.navigate("http://localhost:8083/pages/App/pages/login.sample.html");
+        browser.describe(EditField.class, new EditFieldDescription.Builder()
+                .type("text").tagName("INPUT").name("uname").build()).setValue("Amir");
+        browser.describe(EditField.class, new EditFieldDescription.Builder()
+                .type("password").tagName("INPUT").name("psw").build()).setValue("HPE");
     }
 
     @When("^user clicks on my dashboard$")
@@ -35,7 +44,21 @@ public class FeatureStepDef extends UnitTestClassBase{
     public void dashboard_page_is_displayed() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         Reporter.reportEvent("Step executed", "step executed");
+        Button UserName = browser.describe(Button.class, new ButtonDescription.Builder()
+                .buttonType("submit").tagName("BUTTON").name("Login").build());
 
+        WebElement myLabel = browser.describe(WebElement.class, new WebElementDescription.Builder()
+                .tagName("B").innerText("Username").build());
+        Verify.areEqual(myLabel.getInnerText(),"User");
+        Verify.areEqual(myLabel.getAttribute("value"),"User");
+        Verify.areEqual(myLabel.getAttribute("type"),"User");
+
+        EditField EdtUserName = browser.describe(EditField.class, new EditFieldDescription.Builder()
+                .type("text").tagName("INPUT").name("uname").build());
+        Verify.areEqual(EdtUserName.getAttribute("name"),"User");
+        Verify.areEqual(UserName.getInnerText(),"Login");
+        browser.describe(Button.class, new ButtonDescription.Builder()
+                .buttonType("submit").tagName("BUTTON").name("Login").build()).click();
         Reporter.generateReport();
         SDK.cleanup();
     }
