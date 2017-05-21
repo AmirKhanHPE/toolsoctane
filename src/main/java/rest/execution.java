5be7f344e6969fe663a48e27f522c30aa5d2e127
khanami <amir.khan@hpe.com>
2017-05-21 14:39:12 +0200

    planning poker initial

--- src/main/java/rest/execution.java
+++ src/main/java/rest/execution.java
@@ -11,21 +11,21 @@ public class execution {
     public static void main(String[] args)  throws IOException
     {
        Octane.Builder octane= readSDK2writeDATA.Login2Octane("https://mqast001pngx.saas.hpe.com",
-               "Amir_Khan_API_46zgyv4sedn443oyevl",
-               "+bf48cc89394Q",
+               "Amir_Khan_API_46zgykz6lq8v4sedn443oyevl",
+               "+bf4c8fb48cc89394Q",
                2004);
 
        //octane.signOut();
 
 
-        octane = readSDK2writeDATA.GetNodes2File(octane,2004,10001);
-        octane = readSDK2writeDATA.GetPhases2File(octane,2004,11001);
+        octane = readSDK2writeDATA.GetNodes2File(octane,2004,20001);
+        octane = readSDK2writeDATA.GetPhases2File(octane,2004,20001);
         //octane = readSDK2writeDATA.GetReleases2File(octane,2004,10001);
-        octane = readSDK2writeDATA.GetNodes2File(octane,2004,11001);
+        octane = readSDK2writeDATA.GetNodes2File(octane,2004,20001);
         //octane = readSDK2writeDATA.GetEpics2File(octane,2004,10001);
         //octane = readSDK2writeDATA.GetFeatures2File(octane,2004,10001);
-        //octane = readSDK2writeDATA.GetStories2File(octane,2004,10001);
-        octane =readSDK2writeDATA.GetDefects2File(octane,2004,11001);
+        octane = readSDK2writeDATA.GetStories2File(octane,2004,20001);
+        //octane =readSDK2writeDATA.GetDefects2File(octane,2004,11001);
         //octane =readSDK2writeDATA.GetManualTests2File(octane,2004,10001);
         //octane =readSDK2writeDATA.GetGherkinTests2File(octane,2004,10001);
         //octane =readSDK2writeDATA.GetTestSuites2File(octane,2004,10001);
